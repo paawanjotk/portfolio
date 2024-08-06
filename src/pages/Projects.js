4da4873ebd01079d55c355bae16de3ca352e1bee
paawanjotk <145671665+paawanjotk@users.noreply.github.com>
2024-08-06 15:07:37 +0530

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className='h-screen bg-[#010e1b] text-white flex flex-col items-center pt-8'>
         <div className=' flex flex-col align-middle justify-center gap-10  w-2/5'>
-            <h4 className=' text-2xl'>Projects----------</h4>
+            <h4 className=' text-2xl'>Projects</h4>
             <div className=' gallery-cards grid grid-cols-2 gap-10'>
                 <div>
                   <p className=' text-2xl font-mono font-bold'>Movie Ticket Booking System- BackEnd</p>
