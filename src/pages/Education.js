066a7d8c191b1aae5ec920be87cd3995c6f7ddac
paawanjotk <145671665+paawanjotk@users.noreply.github.com>
2024-08-06 15:07:23 +0530

    Update Education.js

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -4,7 +4,7 @@ const Education = () => {
   return (
         <div className=' h-screen bg-[#010e1b] text-white flex flex-col items-center pt-8 '>
             <div className=' flex flex-col align-middle justify-center gap-10  w-2/5'>
-                <h4 className=' text-2xl'>Education----------</h4>
+                <h4 className=' text-2xl'>Education</h4>
                 <div className=' text-white tile-container flex flex-row gap-10'>
                     <img className=' w-80' src="https://scoonews.com/wp-content/uploads/2023/12/WhatsApp-Image-2023-12-08-at-11.12.47-AM-900x600.jpeg" alt="Scaler School of Technology"/>
                     <div>
